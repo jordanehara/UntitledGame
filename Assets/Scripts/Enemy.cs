bfdf5a811da878e8cba85ee3f41a29dbbd945343
Jordan Ehara <jordan.ehara@gmail.com>
2023-12-11 21:09:31 -0800

    destroy enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -36,4 +36,27 @@ public class Enemy : MonoBehaviour
         Destroy(this.gameObject, this.maxLifetime);
     }
 
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            if ((this.size * 0.5) >= minSize)
+            {
+                CreateSplit();
+                CreateSplit();
+            }
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void CreateSplit()
+    {
+        Vector2 position = this.transform.position;
+        position += Random.insideUnitCircle * 0.5f;
+
+        Enemy half = Instantiate(this, position, this.transform.rotation);
+        half.size = this.size * 0.5f;
+
+        half.SetTrajectory(Random.insideUnitCircle.normalized * this.speed);
+    }
 }
