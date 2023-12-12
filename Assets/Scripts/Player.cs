f23c7f3e1399c1ceafca6578863ae5858f7519fe
Jordan Ehara <jordan.ehara@gmail.com>
2023-12-11 20:33:39 -0800

    shoot bullet

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,7 +11,8 @@ public class Player : MonoBehaviour
     public float thrustSpeed = 1.0f;
     public float turnSpeed = 1.0f;
 
-    // Initialization work
+    public Bullet bulletPrefab;
+
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
@@ -19,6 +20,7 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        // Player move
         _thrusting = (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow));
 
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
@@ -29,8 +31,11 @@ public class Player : MonoBehaviour
 
         else
             _turnDirection = 0;
-    }
 
+        // Shoot
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+            Shoot();
+    }
 
     private void FixedUpdate()
     {
@@ -43,6 +48,12 @@ public class Player : MonoBehaviour
         {
             _rigidbody.AddTorque(_turnDirection * this.turnSpeed);
         }
+    }
+
+    private void Shoot()
+    {
+        Bullet bullet = Instantiate(this.bulletPrefab, this.transform.position, this.transform.rotation);
+        bullet.Project(this.transform.up);
     }
 
 }
